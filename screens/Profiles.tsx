82c5a44e92365bcce415d02825dd4162e7bca726
Cubas-G <fredyscubas00@gmail.com>
2022-12-07 21:25:17 -0600

    creacion de las vistas principales de la app

---
+++ screens/Profiles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Profiles = () => {
+  return (
+    <View>
+      <Text>Profiles</Text>
+    </View>
+  )
+}
+
+export default Profiles
+
+const styles = StyleSheet.create({})
